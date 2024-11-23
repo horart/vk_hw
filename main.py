44dced483de0cc5b1e4c7f2e314c7c2ef458e86b
Artyom <artem.khorev7@ya.ru>
2024-11-23 15:15:08 +0300

    added age checking

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ with open("config.json") as f:
 	config = json.load(f)
 
 
+if config["users"][0]["age"] < 18:
+	exit("Users younger than 18 are not allowed!")
+
 print("Hello world,", config["users"][0]["name"])
+
