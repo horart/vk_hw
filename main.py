1272061a8a51acf3ea24ea7ea01a74212475cfda
Artyom <artem.khorev7@ya.ru>
2024-11-23 15:22:09 +0300

    added language support

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with open("config.json") as f:
 
 
 if config["users"][0]["age"] < 18:
-	exit("Пользователям до 18 нельзя!")
+	exit("Пользователям до 18 нельзя!" if config["lang"] == "ru" else "no users under 18!")
 
-print("Hello world,", config["users"][0]["name"])
+print("привет мир," if config["lang"] == "ru" else "hello world, ", config["users"][0]["name"])
 
