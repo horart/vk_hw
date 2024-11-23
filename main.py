ca1a6df1c54c75cb1ffc144261f2faf2b8436403
Artyom <artem.khorev7@ya.ru>
2024-11-23 15:25:16 +0300

    added an excl mark

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ with open("config.json") as f:
 if config["users"][0]["age"] < 18:
 	exit("Пользователям до 18 нельзя!" if config["lang"] == "ru" else "no users under 18!")
 
-print("привет мир," if config["lang"] == "ru" else "hello world, ", config["users"][0]["name"])
+print("привет мир," if config["lang"] == "ru" else "hello world, ", config["users"][0]["name"], "!")
 
